4fa856baa07919232a0aee758f5278631cddc9b2
Ramon <ramon.r.santana@gmail.com>
2023-03-20 14:18:56 +0100

    freat: get information in login views from backend

--- Dental-Town/src/main.jsx
+++ Dental-Town/src/main.jsx
@@ -5,13 +5,22 @@ import { App } from './App';
 
 
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import { BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from './App/store';
+
+
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>   
     </BrowserRouter>
    
   </React.StrictMode>,
